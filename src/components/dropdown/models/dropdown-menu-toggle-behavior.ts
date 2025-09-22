654d31f4c6c3dae086121a6dc7430d0bf0ee03b8
igorfilippov3 <filippovigorz@gmail.com>
2025-09-22 10:15:34 +0200

    2

--- src/components/dropdown/models/dropdown-menu-toggle-behavior.ts
+++
@@ -1,4 +0,0 @@
-export enum DropdownMenuToggleBehaviour {
-  AUTO = 'AUTO', // dropdown панель сркывается/отображается просто по клику на кнопку
-  CUSTOM = 'CUSTOM', // сами решаем, когда скрыть/показать dropdown панель
-}
